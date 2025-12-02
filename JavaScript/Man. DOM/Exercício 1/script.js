747bfd7d0e517c86e8d80ff5ccb3896e4b00c493
joao-riffel <joaovicenteriffel05@gmail.com>
2025-12-02 17:23:34 -0300

    Exercício 2 DOM Iniciado
    O exercício de manipulação de dom foi iniciado porém não concluído

---
+++ JavaScript/Man. DOM/Exercício 1/script.js
@@ -0,0 +1,26 @@
+const btnAlterarUm = document.getElementById("btnAlterarUm");
+
+btnAlterarUm.addEventListener("click", function () {
+  const paragrafoUm = document.getElementById("paragrafoUm");
+
+  paragrafoUm.textContent =
+    "Em uma realidade alternativa, o Brasil nunca deixou de ser um império. A tecnologia avançou guiada por inventores financiados pela coroa, e o país se tornou referência em transportes aéreos movidos a energia solar. Nas cidades, dirigíveis dourados cruzam o céu enquanto cidadãos convivem com tradições antigas e inovações futuristas.";
+});
+
+const btnAlterarDois = document.getElementById("btnAlterarDois");
+
+btnAlterarDois.addEventListener("click", function () {
+  const paragrafoDois = document.getElementById("paragrafoDois");
+
+  paragrafoDois.textContent =
+    "Em outra linha do tempo, a humanidade descobriu vida inteligente no fundo do oceano antes de chegar ao espaço. Civilizações aquáticas, feitas de corais vivos e bioluminescência, ensinaram aos humanos novas formas de comunicação baseadas em ondas sonoras. Essa aliança transformou mares antes explorados em santuários compartilhados entre espécies.";
+});
+
+const btnAlterarTres = document.getElementById("btnAlterarTres");
+
+btnAlterarTres.addEventListener("click", function () {
+  const paragrafoTres = document.getElementById("paragrafoTres");
+
+  paragrafoTres.textContent =
+    "Numa terceira versão do mundo, o tempo deixou de correr de forma linear. Pessoas podem reviver dias especiais, corrigir erros ou experimentar novos caminhos, mas cada escolha altera discretamente o tecido da realidade. Com isso, surgiram “navegadores temporais”, especialistas em evitar paradoxos e manter o equilíbrio entre todas as versões possíveis do mundo.";
+});
---
+++ JavaScript/Man. DOM/Exercício 2/script.js
@@ -0,0 +1,9 @@
+const btnAparecer = document.getElementById("btnAparecer");
+const quadrado = document.getElementById("quadrado");
+
+btnAparecer.addEventListener("click", function (){
+   
+    classList.toggle("quadrado");
+
+
+})
