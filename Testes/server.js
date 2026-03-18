a25ec6be1f4f06c9cfee46e375e9354c04568da3
joao-riffel <joaovicenteriffel05@gmail.com>
2026-03-18 16:21:54 -0300

    Conectando Front e Back
    Feito até a conexão com o Banco de Dados. A partir de agora serão feitos exercícios de manutenção para fixação

---
+++ Testes/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const usuariosRoutes = require("./routes/usuarios");
+
+app.use(express.json());
+app.use(express.static("public"));
+
+// Prefixo da API
+app.use("/usuarios", usuariosRoutes);
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
